ff4ccdd4978f569a2f6401083712d87f72702c1e
shofukuhara <sho.fukuhara0213@gmail.com>
2024-10-29 23:14:54 +0900

    add:画面幅・高さ設定

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,6 @@
-window.addEventListener("DOMContentLoaded", () => {
+import { _updateViewportSize } from './modules/_viewport';
+
+window.addEventListener('DOMContentLoaded', () => {
   // 共通処理
+  _updateViewportSize();
 });
