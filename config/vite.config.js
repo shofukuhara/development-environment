d2856b9d099297cb0171b82efcda15daee837b98
shofukuhara <sho.fukuhara0213@gmail.com>
2024-11-09 15:27:13 +0900

    fix:ファイル調整

--- config/vite.config.js
+++ config/vite.config.js
@@ -2,83 +2,70 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import globule from 'globule';
 import vitePluginPugStatic from '@macropygia/vite-plugin-pug-static';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-import license from "rollup-plugin-license";
+import license from 'rollup-plugin-license';
 import { terser } from 'rollup-plugin-terser';
 import fs from 'fs';
 
-// `src` ディレクトリ内の Pug ファイルを取得
-const inputs = {};
-const documents = globule.find([path.resolve(__dirname, '../src/**/*.pug')], {
-  ignore: [path.resolve(__dirname, '../src/**/_*.pug')],
-});
+// `src` ディレクトリ内の Pug ファイルを取得して出力名を作成
+const getPugInputs = () => {
+  const documents = globule.find([path.resolve(__dirname, '../src/**/*.pug')], {
+    ignore: [path.resolve(__dirname, '../src/**/_*.pug')],
+  });
+  return documents.reduce((acc, document) => {
+    const relativePath = path.relative(path.resolve(__dirname, '../src'), document);
+    const outputName = relativePath.replace(/\.pug$/, '.html');
+    acc[outputName] = document;
+    return acc;
+  }, {});
+};
 
-// Pugファイルをdistに出力
-documents.forEach((document) => {
-  const fileName = path.relative(path.resolve(__dirname, '../src'), document);
-  const outputName = fileName.replace(/\.pug$/, '.html');
-  inputs[outputName] = document;
-});
-
-// CSS と JS だけをビルドするための出力設定
+// 出力オプション設定
 const outputOptions = {
   entryFileNames: 'assets/js/index.js',
   chunkFileNames: 'assets/js/index.js',
   assetFileNames: (assetInfo) => {
-    if (/\.(gif|jpeg|jpg|png|svg|webp)$/.test(assetInfo.name)) {
-      return 'assets/images/[name].[ext]';
-    }
-    if (/\.css$/.test(assetInfo.name)) {
-      return 'assets/css/[name].[ext]';
-    }
+    if (/\.(gif|jpeg|jpg|png|svg|webp)$/.test(assetInfo.name)) return 'assets/images/[name].[ext]';
+    if (/\.css$/.test(assetInfo.name)) return 'assets/css/[name].[ext]';
     return 'assets/[name].[ext]';
   },
 };
 
+// ライセンスコメント追加プラグイン
+const addLicenseComment = () => ({
+  name: 'add-license-comment',
+  writeBundle: () => {
+    const filePath = path.resolve(__dirname, '../dist/assets/js/index.js');
+    const comment = '/*! Please refer to licence.txt for the details of the license. */\n';
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) return console.error(err);
+      const modifiedData = comment + data;
+      fs.writeFile(filePath, modifiedData, 'utf8', (err) => err && console.error(err));
+    });
+  },
+});
+
 export default defineConfig({
-  root: path.resolve(__dirname, '../src'), // `src` ディレクトリから見た相対パス
+  root: path.resolve(__dirname, '../src'),
   base: './',
   build: {
-    outDir: path.resolve(__dirname, '../dist'), // ビルド出力先
+    outDir: path.resolve(__dirname, '../dist'),
     emptyOutDir: true,
     minify: 'terser',
+    // jsのビルド設定
     terserOptions: {
-      format: {
-        comments: false,
-      },
-      compress: {
-        drop_console: true,
-      },
+      // コメント削除
+      format: { comments: false },
+      // consoleの削除
+      compress: { drop_console: true },
     },
     rollupOptions: {
-      input: inputs, // Pug からのエントリーポイントを指定
+      input: getPugInputs(),
       output: outputOptions,
-      plugins: [
-        {
-          name: 'add-license-comment',
-          writeBundle: () => {
-            const filePath = path.resolve(__dirname, '../dist/assets/js/index.js');
-            const comment = '/*! Please refer to licence.txt for the details of the license. */\n';
-            fs.readFile(filePath, 'utf8', (err, data) => {
-              if (err) {
-                console.error(err);
-                return;
-              }
-              // 先頭にコメントを追加する
-              const modifiedData = comment + data;
-              fs.writeFile(filePath, modifiedData, 'utf8', (err) => {
-                if (err) {
-                  console.error(err);
-                }
-              });
-            });
-          },
-        },
-        terser(),
-      ],
+      plugins: [addLicenseComment(), terser()],
     },
   },
   plugins: [
+    // pugの開発・ビルド設定
     vitePluginPugStatic({
       buildOptions: { basedir: path.resolve(__dirname, '../src') },
       serveOptions: { basedir: path.resolve(__dirname, '../src') },
@@ -86,15 +73,17 @@ export default defineConfig({
     // 使用しているライブラリのライセンス出力
     license({
       thirdParty: {
-        output: path.join(__dirname, "../dist/assets/js/license.txt"),
-        includePrivate: true, // Default is false.
+        output: path.join(__dirname, '../dist/assets/js/license.txt'),
+        includePrivate: true,
       },
     }),
     // ビルドしたディレクトリをコピー
     // viteStaticCopy({
     //   targets: [
     //     {
+    //       // 出力元
     //       src: path.join(__dirname, '../dist/assets/css'),
+    //        // 出力先
     //       dest: path.resolve(__dirname, '')
     //     },
     //     {
@@ -104,6 +93,10 @@ export default defineConfig({
     //   ],
     // }),
   ],
+  // 開発時source map出力
+  css: { devSourcemap: true },
+  postcss: path.resolve(__dirname, './postcss.config.cjs'),
+  // ローカルサーバー立ち上げ
   server: {
     open: true,
     host: true,
