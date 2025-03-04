3bfb20884803dc9d9a9cba4cbd3c16e754bceccf
shofukuhara <sho.fukuhara0213@gmail.com>
2025-03-05 01:32:33 +0900

    fix:調整

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,26 +1,21 @@
 import { UpdateViewportSize } from "./utils/viewport-utils";
 import { BreakpointObserver } from "./utils/breakpoint-utils";
-
-const page = document.querySelector("[data-page]");
-const pageAttribute = page.getAttribute("data-page");
-
-// ================
-// 共通
-// ================
+// 共通処理格納
 const commonModules = {
-  viewport: new UpdateViewportSize(),
+  common: () => {},
 };
 
-Object.values(commonModules).forEach((module) => module.init());
-
-// ================
-// 各ページ
-// ================
+// 各ページごとの処理格納
 const pageModules = {
-  top: UpdateViewportSize,
+  // home: () => {},
 };
 
-if (pageAttribute in pageModules) {
-  const instance = new pageModules[pageAttribute]();
-  instance.init();
+// 各ページの処理チェック
+const page = document.querySelector("[data-page]");
+if (page) {
+  const pageAttribute = page.getAttribute("data-page");
+  commonModules.common(); // 共通処理を実行
+  if (pageModules[pageAttribute]) {
+    pageModules[pageAttribute](); // 各ページの処理を実行
+  }
 }
